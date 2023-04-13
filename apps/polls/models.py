e6613fada316657bcefc968b0411cadf5f71ddb7
amerkurev <apps.merkurev@gmail.com>
2023-04-13 14:20:59 +0300

    Initial commit

---
+++ apps/polls/models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text

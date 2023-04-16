a183546792be37774614a5d8beb2039cf9dabb30
amerkurev <apps.merkurev@gmail.com>
2023-04-17 02:34:43 +0300

    add specific directory for sqlite file

--- website/website/settings.py
+++ website/website/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path(os.environ['DJANGO_SQLITE_DIR']) / 'db.sqlite3',
     }
 }
 

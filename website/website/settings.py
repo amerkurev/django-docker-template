510c660c4e7461780b890984f75cb3d6a059fa76
amerkurev <apps.merkurev@gmail.com>
2023-04-24 22:24:21 +0300

    improved project structure to follow Django conventions

--- website/website/settings.py
+++ website/website/settings.py
@@ -10,14 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import sys
 
 from pathlib import Path
 from website import is_true, split_with_comma
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -211,7 +209,6 @@ LOGGING = {
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
-            'stream': sys.stdout,
         },
         'mail_admins': {
             'level': 'ERROR',

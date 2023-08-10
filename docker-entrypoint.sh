c64124b1288d8fbb733eae7a0dd02dc7baa893e5
amerkurev <apps.merkurev@gmail.com>
2023-08-10 11:37:43 +0300

    add comment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ if [ -z ${POSTGRES_DB+x} ]; then
 else
   wait-for-it -s "$POSTGRES_HOST:$POSTGRES_PORT" -t 60
 fi
+# You can comment out this line if you want to migrate manually
 su-exec "$USER" python manage.py migrate --noinput
 
 exec "$@"

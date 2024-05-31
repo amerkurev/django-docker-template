d038dbfe5374019c378da7d2a84b2ea68c822729
nbovee <12849851+nbovee@users.noreply.github.com>
2024-05-31 02:15:17 -0400

    vscode integration, safe superuser command (+app)

--- website/website/settings.py
+++ website/website/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'setup.apps.SetupConfig',
     'polls.apps.PollsConfig',
 ]
 
